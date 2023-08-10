989023d09d0e1d4d50cebe150bbe96a8b32696f4
pakitowka <whitesaint12@gmail.com>
2023-08-10 19:48:18 +0200

    Implementing mail sending

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
-import requests
+import yagmail
 from dotenv import load_dotenv
-import os
 import typing
+import os
+import pandas as pd
+from news import NewsFeed
 
+load_dotenv()
+MAIL_LOGIN: typing.Final = os.getenv("MAIL_LOGIN")
+MAIL_PASS: typing.Final = os.getenv("MAIL_PASSWORD")
 
-class NewsFeed:
-    """This class represents news that are for sending on e-mail"""
+df = pd.read_excel('files/people.xlsx')
 
-    load_dotenv()
-    API_KEY: typing.Final = os.getenv("API_KEY")
-    base_url = 'https://newsapi.org/v2/everything'
+for index, row in df.iterrows():
+    receiver = row['email']
+    topic = row['interest']
+    receiver_name = row['name']
+    receiver_surname = row['surname']
+    news_feed = NewsFeed(topic=topic, from_date='2023-8-9', to_date='2023-8-10')
 
-    def __init__(self, topic, from_date, to_date, language):
-        self.topic = topic
-        self.from_date = from_date
-        self.to_date = to_date
-        self.language = language
+    subject_body = f'Your {topic} news for today!'
+    message_body = f"Hi, {receiver_name} {receiver_surname}\n see what's about {topic} today." \
+                   f"\n{news_feed.get_news()}\nAndrew"
 
-    def get_news(self):
-        """converts json from request into formatted string for sending email"""
-        params = {'qInTitle': self.topic,
-                  'from': self.from_date,
-                  'to': self.to_date,
-                  'language': self.language,
-                  'apiKey': self.API_KEY}
-        response = requests.get(self.base_url, params=params)
-        content = response.json()
-        articles = content['articles']
-        email_body = ''
-        for article in articles:
-            email_body = email_body + f"{article['title']}\n{article['url']}\n\n"
-        return email_body
-
-# test
-# news_feed = NewsFeed(topic='Nasa', from_date='2023-08-07', to_date='2023-08-08', language='en')
-# print(news_feed.get_news())
-
+    email = yagmail.SMTP(user=MAIL_LOGIN, password=MAIL_PASS)
+    email.send(to=receiver,
+               subject=subject_body,
+               contents=message_body)
